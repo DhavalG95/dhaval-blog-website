cccfa92e7e140533504eea7ae652f2326f3a82c8
Dhaval Gothi <dhavalgothir@gmail.com>
2023-01-06 19:30:33 +0530

    add javascripts

---
+++ app1/static/scripts/first.js
@@ -0,0 +1,7 @@
+function reveal()
+{
+if(document.getElementById('box').checked)
+   {document.getElementById("id_password").type='text';}
+else
+document.getElementById("id_password").type='password';
+}
